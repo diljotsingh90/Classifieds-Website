ebe227c4959c0568c081fbc4cb743ebabd49af38
diljotsingh90 <diljotsingh90@yahoo.com>
2020-07-03 13:22:18 +0530

    hh

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
+
+const { Sequelize } = require("sequelize"); // mysql database related
+const sequelize = new Sequelize("classifieds", "root", "admin", {
+  host: "localhost",
+  dialect: "mysql",
+});
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully."))
+  .catch((error) => {
+    console.log("Unable to connect to the database:", error);
+    /* throw new Error(error); */
+  });
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.render("error404");
+});
+
+app.use((error, req, res, next) => {
+  res.render("error500");
+});
+
 app.listen(8000);
